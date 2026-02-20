cb42d2a30ed08c24c1651313fb7943c4a6a1f387
Mashael16 <msalih6@outlook.sa>
2026-02-20 04:04:14 +0300

    Add unit test for Task model

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Task
 
-# Create your tests here.
+class TaskModelTest(TestCase):
+    def setUp(self):
+        # Setting up a dummy task for testing
+        self.task = Task.objects.create(
+            title="CI System Test",
+            description="Testing GitHub Actions workflow"
+        )
+
+    def test_task_creation(self):
+        # Verifying that the task was created correctly
+        self.assertEqual(self.task.title, "CI System Test")
+        self.assertTrue(isinstance(self.task, Task))
