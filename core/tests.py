59165858a5b335c577bf2ffc5e4c5bcd68034dea
Mashael16 <msalih6@outlook.sa>
2026-02-20 04:53:10 +0300

    fix test for evalution

--- core/tests.py
+++ core/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.utils import timezone
 from django.contrib.auth import get_user_model
-from .models import Task
+from .models import Task, Evaluation
 
 User = get_user_model()
 
@@ -26,3 +26,37 @@ class TaskModelTest(TestCase):
         self.assertEqual(self.task.title, "CI System Test")
         self.assertEqual(self.task.assigned_to.username, "testuser")
         self.assertTrue(isinstance(self.task, Task))
+
+
+class PerformancePlatformTest(TestCase):
+    def setUp(self):
+        
+        self.user = User.objects.create_user(
+            username="mashael_dev",
+            password="securepassword",
+            role="employee"
+        )
+
+        
+        self.task = Task.objects.create(
+            title="Final Project CI",
+            description="Testing full workflow",
+            assigned_to=self.user,
+            deadline=timezone.now() + timezone.timedelta(days=1)
+        )
+
+    def test_task_creation(self):
+        self.assertEqual(self.task.title, "Final Project CI")
+        self.assertTrue(isinstance(self.task, Task))
+
+    def test_evaluation_link(self):
+        
+        evaluation = Evaluation.objects.create(
+            task=self.task,
+            evaluator=self.user,
+            objective_score=95.0,
+            subjective_score=90.0,
+            feedback="Excellent progress in CI/CD implementation"
+        )
+        self.assertEqual(evaluation.task.title, "Final Project CI")
+        self.assertEqual(self.task.evaluation.objective_score, 95.0)
